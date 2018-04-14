a979d3c114d5e76ed6bb7868051c1de4773354c1
Ankit Kalia <ankit.kalia@vinsol.com>
2018-04-14 21:34:00 +0530

    add jquery and bootstrap, modify devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   use_doorkeeper scope: 'api/v1/oauth'
-  # devise_for :users
+  devise_for :users, only: [:sessions, :registrations], controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+
   resources :links, only: :index
 
   root 'links#index'
 
   namespace :api do
     namespace :v1 do
-
       resources :links, only: :index
     end
   end
